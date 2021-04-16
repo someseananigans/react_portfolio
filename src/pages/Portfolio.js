da9a1b4d1b1d89c4fdbbb58bf550b6f6de225793
Sean Nguyen <nsean321@gmail.com>
2021-04-15 22:50:40 -0700

    portfolio structure built (needs styling and projects info)

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,22 +1,40 @@
-import './css/portfolio.css'
-import useBodyColor from './hooks/changeBgColor'
+import useBodyColor from '../hooks/useBodyColor'
+import Project from '../components/Project'
 
-
-const Portfolio = () => {
+const Portfolio = ({colorScheme, projects}) => {
   useBodyColor('darkblue')
   return (
-    <div className="wrapper2">
-      <div className="card1">
-        <aside className="description">
-          <img className="image" src="https://images.unsplash.com/photo-1468276311594-df7cb65d8df6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt="img1" />
+    <>
+      {/* <Display>
+        <Container>
+          <Row>
+            <SkillColumn>
+              <SkillsWrapper>
+                <Heading></Heading>
+                <SkillsIconWrapper>
+                  <SkillIcons></SkillIcons>
+                </SkillsIconWrapper>
+              </SkillsWrapper> */}
 
-        </aside>
+              {/* <ExtrasWrapper>
 
-      </div>
+              </ExtrasWrapper> */}
 
-
-    </div>
+            {/* </SkillColumn>
+            <ProjectColumn>
+              <ProjectWrapper>
+                <Project />
+              </ProjectWrapper>
+            </ProjectColumn>
+          </Row>
+        </Container>
+      </Display> */}
+      <Project />
+    </>
   )
 }
 
 export default Portfolio
+
+
+// make card1 position relative and aside position absolute
