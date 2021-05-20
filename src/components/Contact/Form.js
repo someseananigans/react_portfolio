a5252f603374c645c24991d3cad46b3d1a94f88e
Sean Nguyen <nsean321@gmail.com>
2021-05-19 22:42:59 -0700

    added modal post submit

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -6,11 +6,13 @@ import TextField from '@material-ui/core/TextField';
 // import Button from '@material-ui/core/Button'
 import { colorPalette } from '../../Data';
 import emailjs from 'emailjs-com'
+import { FaTimes } from 'react-icons/fa'
+
 
 const { colorScheme } = colorPalette
 
 const Form = (colorScheme) => {
-
+  const [isOpen, setOpen] = useState(false)
   const [formState, setFormState] = useState({
     name: {
       value: '',
@@ -25,8 +27,41 @@ const Form = (colorScheme) => {
       error: ''
     }
   })
+  const [dialogue, setDialogue] = useState({
+    primary: '',
+    secondary: ''
+  })
+  const submitted = 'Thank you for your submission!'
+  const errored = `Looks like there's something missing...`
+  const errored2 = `Please try again`
 
-  const handleValidation = () => {
+  const handleOpen = (success) => {
+    if (success) {
+      setDialogue({ primary: submitted, secondary: '' })
+    } else {
+      setDialogue({ primary: errored, secondary: errored2 })
+    }
+    setOpen(true)
+    setTimeout(() => {
+      setOpen(false)
+    }, 200000);
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const sendEmail = () => {
+    emailjs.send("service_ld92v8k", "template_am2pbum", {
+      name: formState.name.value,
+      email: formState.email.value,
+      message: formState.message.value
+    }, 'user_MtxWh9NUZ8TCL3fwtGgwM')
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // Validation
     let eErrText, nErrText, mErrText = ''
     let emailValid = formState.email.value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
     if (!emailValid) { eErrText = `Hmm... I can't find your email` }
@@ -34,7 +69,7 @@ const Form = (colorScheme) => {
     if (!contName) { nErrText = `Sorry, I didn't get your name` }
     let contMessage = formState.message.value.length >= 1
     if (!contMessage) { mErrText = `Don't forget the message~` }
-
+    // for Error notice on form
     setFormState({
       ...formState,
       email: {
@@ -50,28 +85,31 @@ const Form = (colorScheme) => {
         error: mErrText
       }
     })
-  }
-
-  const sendEmail = () => {
-    emailjs.send("service_ld92v8k", "template_am2pbum", {
-      name: formState.name.value,
-      email: formState.email.value,
-      message: formState.message.value
-    }, 'user_MtxWh9NUZ8TCL3fwtGgwM')
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    handleValidation()
     //if error do not submit and set error
-    if (formState.email.error || formState.name.error || formState.message.error) {
+    if (mErrText || nErrText || eErrText) {
       console.log('Form is not complete')
-
+      handleOpen(false)
     }
     // set message and contact info to be an email sent to you
     else {
-      sendEmail()
-      console.log('email sent')
+      // sendEmail()
+      handleOpen(true)
+      setTimeout(() => {
+        setFormState({
+          name: {
+            value: '',
+            error: ''
+          },
+          email: {
+            value: '',
+            error: '',
+          },
+          message: {
+            value: '',
+            error: ''
+          }
+        })
+      }, 500);
     }
   }
 
@@ -102,53 +140,68 @@ const Form = (colorScheme) => {
         })
         break;
     }
+  }
 
+  const RenderMessage = () => {
+    return (
+      <Dialogue onClick={((event) => event.stopPropagation())} >
+        <DialoguePrimary>{dialogue.primary}</DialoguePrimary>
+        <DialogueSecondary>{dialogue.secondary}</DialogueSecondary>
+        <Icon onClick={handleClose}>
+          <CloseIcon />
+        </Icon>
+      </Dialogue >
+    )
   }
 
   const { name, message, email } = formState
 
   return (
-    <FormBox color={colorScheme}>
-      <FormWrapper onSubmit={handleSubmit} className='contactForm' noValidate autoComplete="off">
-        <Heading>Contact</Heading>
-        <InputField
-          onChange={handleInputChange}
-          error={name.error}
-          value={name.value}
-          name="name"
-          label="Name"
-          variant="filled"
-          helperText={name.error}
-        />
-        <InputField
-          onChange={handleInputChange}
-          error={email.error}
-          value={email.value}
-          name="email"
-          label="Email"
-          variant="filled"
-          helperText={email.error}
-        />
-        <InputField
-          multiline
-          rows={7}
-          onChange={handleInputChange}
-          error={message.error}
-          value={message.value}
-          name="message"
-          label="Message"
-          variant="filled"
-          helperText={message.error}
-        />
-        <FormBtn
-          variant='outlined'
-          coloroverwrite={colorScheme}
-          onClick={handleSubmit}
-        >
-          Send
-      </FormBtn>
-      </FormWrapper>
-    </FormBox>
+    <>
+      <FormBox color={colorScheme}>
+        <FormWrapper onSubmit={handleSubmit} className='contactForm' noValidate autoComplete="off">
+          <Heading>Contact</Heading>
+          <InputField
+            onChange={handleInputChange}
+            error={name.error}
+            value={name.value}
+            name="name"
+            label="Name"
+            variant="filled"
+            helperText={name.error}
+          />
+          <InputField
+            onChange={handleInputChange}
+            error={email.error}
+            value={email.value}
+            name="email"
+            label="Email"
+            variant="filled"
+            helperText={email.error}
+          />
+          <InputField
+            multiline
+            rows={7}
+            onChange={handleInputChange}
+            error={message.error}
+            value={message.value}
+            name="message"
+            label="Message"
+            variant="filled"
+            helperText={message.error}
+          />
+          <FormBtn
+            variant='outlined'
+            coloroverwrite={colorScheme}
+            onClick={handleSubmit}>
+            Send
+          </FormBtn>
+        </FormWrapper>
+      </FormBox>
+      <DialogueContainer isOpen={isOpen} onClick={handleClose}>
+        <RenderMessage />
+      </DialogueContainer>
+    </>
   )
 }
 
@@ -174,7 +227,7 @@ const CssTextField = withStyles({
       borderBottomColor: colorScheme[3],
     },
     '& .MuiFormLabel-root.Mui-error': {
-      color: '#f44336',
+      color: '#fff',
     },
     '& .MuiFilledInput-underline.Mui-error:after': {
       borderBottomColor: '#f44336',
@@ -185,6 +238,64 @@ const CssTextField = withStyles({
   },
 })(TextField);
 
+const DialogueContainer = styled.aside`
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  display: grid;
+  align-items: center;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+  /* bottom: ${({ isOpen }) => (isOpen ? '0' : '-100%')}; */
+  top: ${({ isOpen }) => (isOpen ? '0' : '100%')};
+`
+
+const Dialogue = styled.div`
+  border-radius: 10px;
+  background: white;
+  height: 150px;
+  width: 300px;
+  place-self: center;
+  box-shadow: 0px 2px 10px -1px gray;
+  display: flex;
+    flex-direction: column;
+    place-content: center;
+`
+
+const DialoguePrimary = styled.h2`
+  font-family: 'Archivo Black',sans-serif;
+  font-size: 27px;
+  line-height: 25px;
+  letter-spacing: -3px;
+  font-weight: 800;
+  color: #1c242fe8;
+  margin-bottom: 10px;
+`
+const DialogueSecondary = styled.h3`
+  color: #12cfc2;
+`
+const CloseIcon = styled(FaTimes)`
+  color: #1c242fe8;
+  height: 22px;
+
+  &:hover {
+    color: #12cfc2;
+  }
+`
+
+const Icon = styled.div`
+  position: absolute;
+  /* top: 1.2rem;
+  right: 1.5rem; */
+  background: transparent;
+  cursor: pointer;
+  ontline: none;
+  top: 43%;
+  left: 59.5%;
+`
 
 const FormWrapper = styled.form`
   display: flex;
@@ -199,7 +310,7 @@ const FormWrapper = styled.form`
 
 const InputField = styled(CssTextField)`
   width: 90%;
-  margin-bottom: 10px !important;
+  margin-bottom: 5px !important;
 `
 
 const FormBtn = styled.button`
@@ -249,4 +360,10 @@ const FormBox = styled.div`
 
 const Heading = styled.h1`
   color: #f7f8fa;
+  font-family: 'Archivo Black', sans-serif;
+  font-size: 50px;
+  line-height: 45px;
+  letter-spacing: -3px;
+  font-weight: 800;
+  margin-bottom: 5px;
 `
